4194b665c9216d4820cbcf05c0295a0f041e7efe
PedroBotteon <pedrobotteon123@gmail.com>
2024-11-22 15:51:36 -0300

    deploy do projeto

---
+++ server.js
@@ -0,0 +1,11 @@
+import express from "express"; // Importa o framework Express para criar a aplicação web
+import routes from "./src/routes/postsRoutes.js";
+
+const app = express(); // Cria uma instância do Express, que será o núcleo da aplicação
+app.use(express.static("uploads"));
+routes(app);
+
+
+app.listen(3000, () => { // Inicia o servidor na porta 3000 e exibe uma mensagem no console quando o servidor estiver ouvindo
+    console.log("Servidor escutando...");
+});
